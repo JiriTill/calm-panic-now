90accea57ea31c3eca3d8a875d5c3a6c68e109b3
Jiri Till <jirik.till@gmail.com>
2025-08-18 13:11:08 +0200

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,12 @@
 import type { Config } from 'tailwindcss'
+import typography from '@tailwindcss/typography'
 
 const config: Config = {
   content: ['./src/**/*.{ts,tsx,md,mdx}'],
   theme: {
     extend: {},
   },
-  plugins: [],
+  darkMode: 'class',
+  plugins: [typography],
 }
-
 export default config
