b062bf136a6df603c359edb4040b9bd02c8f7c1b
Jiri Till <jirik.till@gmail.com>
2025-08-18 12:16:41 +0200

    Create tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,7 @@
+import type { Config } from 'tailwindcss'
+const config: Config = {
+  content: ['./src/**/*.{ts,tsx,md,mdx}'],
+  theme: { extend: {} },
+  darkMode: 'class',
+}
+export default config
