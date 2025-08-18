262e66d7f64bdce1e498ba8efdf7e93dfc54fffd
Jiri Till <jirik.till@gmail.com>
2025-08-18 16:47:26 +0200

    Update KoFiIframe.tsx

--- src/app/ui/KoFiIframe.tsx
+++ src/app/ui/KoFiIframe.tsx
@@ -1,15 +1,16 @@
 export default function KoFiIframe() {
-  const src =
-    "https://ko-fi.com/calmpanicnow/?hidefeed=true&widget=true&embed=true";
+  const src = "https://ko-fi.com/calmpanicnow/?hidefeed=true&widget=true&embed=true";
 
   return (
-    <iframe
-      id="kofiframe"
-      src={src}
-      title="calmpanicnow"
-      loading="lazy"
-      style={{ border: "0", width: "100%", background: "transparent" }}
-      height={712}
-    />
+    <div className="mx-auto w-full max-w-[460px]">
+      <iframe
+        id="kofiframe"
+        src={src}
+        title="calmpanicnow"
+        loading="lazy"
+        style={{ border: "0", width: "100%", background: "transparent" }}
+        height={620} 
+      />
+    </div>
   );
 }
